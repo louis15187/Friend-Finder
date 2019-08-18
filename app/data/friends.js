beaca6c3edb6c93927a8e7aaa9802d16e5c50e0d
Louis Malone <lmalone874@gmail.com>
2019-08-17 21:22:03 -0400

    commit some more

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendList = [{
+    name: 'Louis Malone',
+    profilePic: 'https://avatars3.githubusercontent.com/u/50034238?s=400&u=11cd770bc3e2f5fa9ae1d3b27c0a33b7f8a8d7f4&v=4',
+    scores: [
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "3",
+        "3",
+        "4",
+        "5"
+
+    ]
+}]
+
+module.exports = friendList;
